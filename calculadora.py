618eab6f26f90f6bb773925c2360747d5740e1b8
Erick e João <erickgolf@yahoo.com>
2025-06-01 00:09:48 -0300

    calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -2,52 +2,58 @@
 
 def som(a, b):
     return a + b
+
 def sub(a, b):
     return a - b
+
 def multi(a, b):
     return a * b
+
 def div(a, b):
     if b != 0:
         return a / b
-    else: return
+    else:
+        return "Erro: divisão por zero"
 
-# Tipos de equações definido em um def, basicamente é a função do visualg
+while True:
+    print('-------------------------------')
+    print('Calculadora')
+    print('-------------------------------')
+    print('[1] Soma')
+    print('[2] Subtração')
+    print('[3] Multiplicação')
+    print('[4] Divisão')
+    print('[0] Sair')
+    print('-------------------------------')
 
-print('-------------------------------')
-print('Calculadora')
-print('-------------------------------')
-print('[1] Soma')
-print('[2] Subtração')
-print('[3] Multiplicação')
-print('[4] Divisão')
-print('-------------------------------')
+    operação = int(input('Qual equação você deseja fazer? '))
 
-# Estrutura da calculadora
+    if operação == 0:
+        print('Encerrando a calculadora.')
+        break  # Sai do laço
 
-operação = int(input('Qual equação você deseja fazer? '))
-num1 = float(input('Digite o primeiro número. '))
-num2 = float(input('Digite o segundo número. '))
+    if operação not in [1, 2, 3, 4]:
+        print('Opção inválida. Tente novamente.')
+        continue
 
-# Definindo variáveis 
+    num1 = float(input('Digite o primeiro número: '))
+    num2 = float(input('Digite o segundo número: '))
 
-if operação == 1:
-    total = som(num1, num2)
-    equação = f'{num1} + {num2}'
-elif operação == 2:
-    total = sub(num1, num2)
-    equação = f'{num1} - {num2}'
-elif operação == 3:
-    total = multi(num1, num2)
-    equação = f'{num1} * {num2}'
-elif operação == 4:
-    total = div(num1, num2)
-    equação = f'{num1} / {num2}'
-else: print('Erro:', NameError)
+    if operação == 1:
+        total = som(num1, num2)
+        equação = f'{num1} + {num2}'
+    elif operação == 2:
+        total = sub(num1, num2)
+        equação = f'{num1} - {num2}'
+    elif operação == 3:
+        total = multi(num1, num2)
+        equação = f'{num1} * {num2}'
+    elif operação == 4:
+        total = div(num1, num2)
+        equação = f'{num1} / {num2}'
 
-# Codando a calculadora ( conferir se o NameError faz realmente o que eu to pensando )
-
-print('-------------------------------')
-print('Resultado')
-print('-------------------------------')
-print(f'Equação: {equação}\nResultado: {total}')
-print('-------------------------------')
+    print('-------------------------------')
+    print('Resultado')
+    print('-------------------------------')
+    print(f'Equação: {equação}\nResultado: {total}')
+    print('-------------------------------\n')
