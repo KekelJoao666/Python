753fb715bef7283bab076b170048ea254bac418d
Erick e João <erickgolf@yahoo.com>
2025-06-02 14:27:42 -0300

    Add files via upload

---
+++ sexo.py
@@ -0,0 +1,17 @@
+sexos=('gay', 'hétero')
+posicao=['de quatro', 'de ladinho', 'papai e mamae']
+
+
+print(type(sexos))
+print(len(posicao))
+posicao.sort
+print(posicao)
+posicao.pop(2)
+print(posicao)
+posicao[1]=69
+print(posicao)
+
+sexofavorito= {
+    'tipos':'gay',
+    'posicao':'de quatro',
+}
