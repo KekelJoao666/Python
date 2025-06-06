e11f0ce79234c648f794ca0055a23998a27a3fd8
Erick e João <erickgolf@yahoo.com>
2025-06-06 14:02:36 -0300

    Add files via upload

---
+++ classroom.py
@@ -0,0 +1,16 @@
+print('ATIVIDADES PYTHON\n ALUNO:JOÃO PEDRO BARRETO')
+print('\nExercícios diretos e de única validação:')
+
+print('\n1. Peça ao usuário um número e informe se ele é positivo ou negativo.')
+numero=int(input('Escreva seu número: '))
+if numero.isdigit:
+    if numero >0:
+        numero=positivo
+    if numero <0:
+        numero=negativo
+print(f'Seu número é: {numero}')
+
+
+
+
+
